947571780d8cfc9900c8d0687599283187fc4487
Mehar <meharsultana9285@gmail.com>
2025-08-27 11:31:35 +0200

    Add calculator project with test projects and test files

---
+++ TestProject1/CalculatorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Xunit.Sdk;
+
+namespace TestProject1
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            double num1 = 65;
+            double num2 = 65;
+            double expected = 130;
+            double actual = Calculator.Addition.ToAdd(num1,num2);
+            Assert.Equal(expected, actual);
+        }
+        [Theory]
+        [InlineData(42, 36, 78)]
+        [InlineData(10, 21, 31 )]
+        [InlineData(74, 108, 182)]
+        public void AdditionToAddTest(double num1,double num2, double expected)
+        {
+            double actual = Calculator.Addition.ToAdd(num1, num2);
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Test2()
+        {
+            double num1 = 6;
+            double num2 = 0;
+           Assert.Throws<DivideByZeroException>(()=>Calculator.Division.ToDivide(num1,num2));
+        }
+    }
+}
